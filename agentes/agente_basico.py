b8902c61910c8398cc3d7d308de3a0da2f3650d2
alejo <alejo@micorreo.com>
2025-03-26 16:44:08 -0400

    agent two - system prompt commit

--- agentes/agente_basico.py
+++ agentes/agente_basico.py
@@ -1,12 +1,36 @@
 from langgraph.prebuilt import create_react_agent
 from core.config import model
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import SystemMessage, HumanMessage
 from agentes.tools import tavily_search_tool
 
 # tools
 tools = [tavily_search_tool]
 
-graph = create_react_agent(
-    model,
-    tools=tools,
+# graph basico
+graph = create_react_agent(model, tools=tools)
+
+# grahp2
+system_prompt = """
+Eres un agente experto en deportes, te llamas TriplePlay, tus funciones son:
+
+- buscar información sobre deportes: utilizando la herramienta de búsqueda de Tavily
+- generar un resumen de deportes: utilizando la api de tavily 
+
+tus respuestas deben ser cortas y precisas, con informacion relevante
+"""
+
+chat_template_prompt =  ChatPromptTemplate.from_messages(
+    [
+        SystemMessage(content=system_prompt),
+        MessagesPlaceholder(variable_name = "messages"),
+    ]
 )
 
+graph2 = create_react_agent(
+    model,
+    tools=tools,
+    prompt=system_prompt,
+    # state
+    # memoria
+)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from agentes.agente_basico import graph
+from agentes.agente_basico import graph, graph2
 from langchain_core.messages import HumanMessage
 
 app = FastAPI()
@@ -23,3 +23,22 @@ async def agente_basico(prompt: AgenteBasico):
     return {
         "response": messages,
     }
+
+@app.post("/agente-basico-2")
+def agente_basico_2(prompt: AgenteBasico):
+    #crear una lista de mensajes
+    #asignar el mensaje de entrada, usuario
+    user_input = [HumanMessage(content=prompt.prompt)]
+
+    #estado inicial de nuestro
+    user_input = {"messages": user_input}
+
+    #invocar la funcion de la graph2 -> agente
+    response = graph2.invoke(user_input)
+
+    #obtener el ultimo mensaje de la respuesta aimessages
+    messages = response["messages"][-1].content
+    return {
+        "response": messages,
+    }
+    pass
